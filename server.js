55efbf2165b6e840649a01fe782b151516ab3538
vinodrajakondala <vinodraja06@gmail.com>
2016-09-18 10:59:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-  res.send('article one requested and will be served here');
+  res.send('article one requested and will be served here Mr Vinod Raja');
 }); 
 
 app.get('/article-two',function(req,res){
-  res.send('article two requested and will be served here');
+  res.send('article two requested and will be served here Mr Vinod Raja Kondala');
 }); 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
